395a10bb5e411c8bf8b622b05475e15de4091951
Ariful islam <arifulbgt4@gmail.com>
2023-10-25 22:51:50 +0600

    dom

---
+++ script.js
@@ -0,0 +1,31 @@
+function GithubAccount(name, email, username) {
+  this.name = name;
+  this.email = email;
+  this.userName = username;
+  this.repo = [];
+
+  this.updateAccount = function (fullName, email) {
+    if (fullName !== undefined) {
+      this.name = fullName;
+    }
+    if (email !== undefined) {
+      this.email = email;
+    }
+  };
+
+  this.getRepos = function () {
+    return this.repo;
+  };
+
+  this.createRepo = function (repoName, creator) {
+    const repo = {
+      name: repoName,
+      creator: creator,
+      createAt: Date.now(),
+    };
+
+    this.repo.push(repo);
+  };
+}
+
+const user = new GithubAccount("Ariful islam", "me@email.com", "arifulbgt4");
