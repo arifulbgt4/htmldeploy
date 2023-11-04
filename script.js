627c525b43e702f7230d3520f4e60ce66e1aa07f
Ariful islam <arifulbgt4@gmail.com>
2023-11-04 21:53:06 +0600

    re-render

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+const app = document.getElementById("index");
+
+const h31 = document.createElement("h3");
+const ul1 = document.createElement("ul");
+const h32 = document.createElement("h3");
+const ul2 = document.createElement("ul");
+h31.innerText = "Open Tasks";
+app.appendChild(h31);
+app.appendChild(ul1);
+h32.innerText = "Completed tasks";
+app.appendChild(h32);
+app.appendChild(ul2);
+
+function getList(open, completed) {
+  const openList = open.map((d, i) => {
+    return `<li onclick="handleComplete(${i})">${d}</li>`;
+  });
+  ul1.innerHTML = openList;
+
+  const completedList = completed.map((d, i) => {
+    return `<li onclick="handleOpen(${i})"><del>${d}</del></li>`;
+  });
+  ul2.innerHTML = completedList;
+}
+
 class ToDo {
   constructor() {
     this.open = [];
@@ -9,15 +34,15 @@ class ToDo {
   }
 
   complete(index) {
-    console.log("first in: ", index);
     this.completed.push(this.open[index]);
     this.open.splice(index, 1);
+    getList(this.open, this.completed);
   }
 
   onopen(index) {
-    console.log("first in: ", index);
     this.open.push(this.completed[index]);
     this.completed.splice(index, 1);
+    getList(this.open, this.completed);
   }
 }
 
@@ -31,39 +56,9 @@ todo.complete(2);
 todo.complete(0);
 todo.onopen(0);
 
-const app = document.getElementById("index");
-
-function createList(open, completed) {
-  const h31 = document.createElement("h3");
-  const ul1 = document.createElement("ul");
-  const h32 = document.createElement("h3");
-  const ul2 = document.createElement("ul");
-
-  h31.innerText = "Open Tasks";
-  app.appendChild(h31);
-  app.appendChild(ul1);
-  open.map((d, i) => {
-    const li = document.createElement("li");
-    li.onclick = () => {
-      todo.complete(i);
-    };
-    li.innerText = d;
-    ul1.appendChild(li);
-  });
-
-  h32.innerText = "Completed tasks";
-  app.appendChild(h32);
-  app.appendChild(ul2);
-  completed.map((d, i) => {
-    const li = document.createElement("li");
-    const del = document.createElement("del");
-    del.innerText = d;
-    li.appendChild(del);
-    li.onclick = () => {
-      todo.onopen(i);
-    };
-    ul2.appendChild(li);
-  });
-}
-
-createList(todo.open, todo.completed);
+const handleComplete = (i) => {
+  todo.complete(i);
+};
+const handleOpen = (i) => {
+  todo.onopen(i);
+};
